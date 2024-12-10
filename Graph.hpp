225079c4c87b437d5cc6b9ea2b402b5a69d0a898
kien050805 <143416413+kien050805@users.noreply.github.com>
2024-12-10 16:22:03 -0500

    init add

---
+++ Graph.hpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <list>
+#include <tuple>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include "customexceptions.hpp"
+
+using namespace std;
+
+class Graph
+{
+private:
+    unordered_map<int, vector<int, double>> Adj;
+    vector<int> V;
+
+public:
+    Graph();
+    Graph(const Graph &G);
+    ~Graph();
+    Graph &operator=(const Graph &G);
+
+    void add_edge(int u, int v, double w);
+    void remove_edge(int u, int v);
+    void delete_vertex(int u);
+    void add_vertex(int u);
+
+    bool check_vertex(int u) const;
+    bool check_edge(int u, int v) const;
+    bool is_empty() const;
+
+    static Graph read_from_STDIN();
+};
+
+#endif
